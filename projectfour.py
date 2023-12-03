9412079e4fbc15ce659eb2a3764678cdb6634ed4
Fijar Akram Fadlullah <fijarakram4@gmail.com>
2023-12-03 22:12:58 +0700

    Menambahkan 5 data pertama dan terakhir

--- projectfour.py
+++ projectfour.py
@@ -5,6 +5,15 @@ import streamlit as st
 st.title("Project Four")
 st.write("## Mini Dashboard SDGs")
 
+df = pd.read_csv('data/inclusive.csv', sep = ';')
+
+st.write("### 5 Data Pertama")
+st.write(df.head() )
+
+st.write("### 5 Data Terakhir")
+st.write(df.tail() )
+
+
 #fadel
 
 

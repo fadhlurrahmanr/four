4e1d68d74f943a9eed662749b1ade90d3ba77f7f
Nindya Syaftita <1302170774.nindyasyaftita@gmail.com>
2023-12-04 21:49:29 +0700

    Mengimpor modul

--- projectfour.py
+++ projectfour.py
@@ -1,29 +1,3 @@
+import streamlit as st
 import pandas as pd
 import plotly.express as px
-import streamlit as st
-
-st.title("Project Four")
-st.write("## Mini Dashboard SDGs")
-
-df = pd.read_csv('data/inclusive.csv', sep = ';')
-
-st.write("### 5 Data Pertama")
-st.write(df.head() )
-
-st.write("### 5 Data Terakhir")
-st.write(df.tail() )
-
-
-#fadel
-
-
-
-#Arina
-
-
-
-#Fijar
-
-
-#Nindya
-
